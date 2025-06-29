f6de1fe35437f03adc53ab81042faa19e6f86937
ogbk <ogbu.k.olu@gmail.com>
2025-06-29 17:23:31 +0100

    edit webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,8 @@ module.exports = {
   module: {
     rules: [
       { test: /\.[jt]{1}sx?$/, use: 'babel-loader', exclude: /(node_modules)/ },
-      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
-      { test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'], exclude: /(node_modules)/ },
+      { test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader'], exclude: /(node_modules)/ },
     ],
   },
 };
