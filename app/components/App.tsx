26de4d0204038538c97e0e6e1b3d66f8b2429678
ogbk <ogbu.k.olu@gmail.com>
2025-07-05 17:52:11 +0100

    switch to hooks

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,135 +1,84 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Preview from './Preview.tsx';
 // import { basicSortObjects } from '../utils/sortBasic';
 import { sortObjects } from '../utils/sortAlphaNum.ts';
 import type {Fields} from '../utils/types';
 
+const initialData: Array<Fields> = require('../utils/data.json');
+const columns = Object.keys(initialData[0]);
 
+const App = () => {
+  const [sortAsc, setSortAsc] = useState(true);
+  const [sortColumn, setSortColumn] = useState(columns[0]);
+  const [data, setData] = useState<Array<Fields>>(sortObjects(initialData, sortColumn, sortAsc));
+  const [selectedRow, setSelectedRow] = useState<Fields>();
 
-type Props = {
-  fields: Array<'id' | 'field1' | 'field2'>,
-  defaultData: Array<Fields>,
-  defaultSortKey: string,
-  defaultSortAsc: boolean
-};
+  const symbolAsc = {
+    true: '\u25B2',
+    false: '\u25BC',
+  };
 
-type State = {
-  sortKey: string,
-  sortAsc: boolean,
-  data: Array<Fields>,
-  previewOn: boolean,
-  selectedRow: Fields,
-  symbolAsc: {
-    true: string,
-    false: string
-  },
-};
+  const sortByColumn = (selectedColumn: string) => {
+    const newSortAsc = ((sortColumn !== selectedColumn) || !sortAsc);
+    const newData = sortObjects(data, selectedColumn, newSortAsc);
 
-export default class App extends Component<Props, State> {
-  state:State = (() => {
-    const { defaultSortKey, defaultSortAsc, defaultData } = this.props;
-    return {
-      sortKey: defaultSortKey,
-      sortAsc: defaultSortAsc,
-      data: sortObjects(defaultData, defaultSortKey, defaultSortAsc),
-      symbolAsc: {
-        true: '\u25B2',
-        false: '\u25BC',
-      },
-      previewOn: false,
-      selectedRow: {},
+    if (newSortAsc !== sortAsc) {
+      setSortAsc(newSortAsc);
     };
-  })();
 
+    if(selectedColumn !== sortColumn) {
+      setSortColumn(selectedColumn);
+    };
 
-  sortByColumn = (newSortKey: string): void => {
-    const { defaultSortAsc, defaultData } = this.props;
-    const {
-      sortKey: currentSortKey,
-      sortAsc: currentSortAsc,
-      data: currentData,
-    } = this.state;
-
-    if (currentSortKey === newSortKey) {
-      this.setState({
-        sortAsc: !currentSortAsc,
-        data: currentData.reverse(),
-      });
-    } else {
-      this.setState({
-        sortAsc: defaultSortAsc,
-        sortKey: newSortKey,
-        data: sortObjects(defaultData, newSortKey, defaultSortAsc),
-      });
-    }
+    setData(newData);
   }
 
-  showPreview = (row: Fields): void => {
-    this.setState({
-      previewOn: true,
-      selectedRow: row,
-    });
-  }
-
-  render() {
-    const { fields: columns } = this.props;
-    const {
-      sortKey,
-      sortAsc,
-      data,
-      symbolAsc,
-      previewOn,
-      selectedRow,
-    } = this.state;
-
-    return (
-      <div id="app">
-        <table>
-          <caption> list </caption>
-          <thead>
-            <tr>
-              {
-                columns.map((columnName) => (
-                  <th
-                    key={`th_${columnName}`}
-                    className={columnName === sortKey ? 'click selected-sort-bar' : 'click'}
-                    onClick={() => { this.sortByColumn(columnName); }}
-                  >
-                    {columnName}
-                    {columnName === sortKey ? symbolAsc[String(sortAsc)] : ' '}
-                  </th>
-                ))
-              }
-            </tr>
-          </thead>
-          <tbody>
+  return (
+    <div id="app">
+      <table>
+        <caption> list </caption>
+        <thead>
+          <tr>
             {
-              data.map((row) => {
-                const { id: rowId } = row;
-                return (
-                  <tr
-                    key={`row_${rowId}`}
-                    className={rowId === selectedRow.id ? 'click clicked-row' : 'click'}
-                    onClick={() => { this.showPreview(row); }}
-                  >
-                    {
-                      columns.map((columnName) => (
-                        <td key={`field_${columnName}`}>
-                          {row[columnName]}
-                        </td>
-                      ))
-                    }
-                  </tr>
-                );
-              })
+              columns.map((columnName) => (
+                <th
+                  key={`th_${columnName}`}
+                  className={columnName === sortColumn ? 'click selected-sort-bar' : 'click'}
+                  onClick={() => { sortByColumn(columnName); }}
+                >
+                  {columnName}
+                  {columnName === sortColumn ? symbolAsc[String(sortAsc)] : ' '}
+                </th>
+              ))
             }
-          </tbody>
-        </table>
-        {
-          previewOn
-          && <Preview rowData={selectedRow} />
-        }
-      </div>
-    );
-  }
-}
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((row) => {
+              const { id: rowId } = row;
+              return (
+                <tr
+                  key={`row_${rowId}`}
+                  className={rowId === selectedRow?.id ? 'click clicked-row' : 'click'}
+                  onClick={() => { setSelectedRow(row); }}
+                >
+                  {
+                    columns.map((columnName) => (
+                      <td key={`field_${columnName}`}>
+                        {row[columnName]}
+                      </td>
+                    ))
+                  }
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
+      { !!selectedRow && <Preview rowData={selectedRow} /> }
+    </div>
+  );
+};
+
+export default App;
--- app/components/index.tsx
+++ app/components/index.tsx
@@ -3,17 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 require('../sass/styles.sass');
 
-const data = require('../utils/data.json');
-
-const fields = Object.keys(data[0]);
-
 // @ts-expect-error
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <App
-    defaultSortKey="id"
-    defaultSortAsc
-    defaultData={data}
-    fields={fields}
-  />
+  <App />
 );
